7290a0a30d8405777949c6a6bc5b853637122f46
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 17:32:06 -0800

    added try except, validate user input for rating

--- ratings.py
+++ ratings.py
@@ -16,11 +16,37 @@ with open(sys.argv[1]) as score_file:
         restaurant = restaurant_rating[0]
         rating = restaurant_rating[1]
         restaurant_ratings[restaurant] = rating
-        
 
-user_restaurant = raw_input("What is the restaurant name? ")
-user_rating = int(raw_input("Please rate this restaurant (1 - 5) "))
-restaurant_ratings[user_restaurant] = user_rating
+
+def is_continue():
+    user_continue = raw_input("Do you want to continue to add another rating? (Y/N) ")
+    if user_continue.lower() == "n":
+        return True
+    elif user_continue.lower() != "y":
+        print "Please enter either Y or N."
+        is_continue()
+
+
+def valid_rating():
+    try:
+        rating = int(raw_input("Please rate this restaurant (1 - 5) "))
+        return rating
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
+        return valid_rating()
+
+
+while True:
+    user_restaurant = raw_input("What is the restaurant name? ")
+    user_rating = valid_rating()
+    #print user_rating
+    if user_rating not in range(1, 6):
+        print "This is not a valid number. Please enter a number between 1 and 5. "
+        valid_rating()
+    restaurant_ratings[user_restaurant] = user_rating
+    if is_continue():
+        break
+
 
 for restaurant, rating in sorted(restaurant_ratings.items()):
     print "{} is rated at {}.".format(restaurant, rating)
