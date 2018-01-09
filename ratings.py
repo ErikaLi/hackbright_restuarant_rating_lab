a9d0d02502caf8e721db7847eeecc1ee5aaf9a3e
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 16:20:16 -0800

    used with open function and ask for user input

--- ratings.py
+++ ratings.py
@@ -4,17 +4,23 @@
 # put your code here
 import sys
 
-score_file = open(sys.argv[1])
 
-restaurant_ratings = {}
+# file closes automatically
+with open(sys.argv[1]) as score_file:
 
-for line in score_file:
-    line = line.rstrip()
-    info = line.split(":")
-    restaurant = info[0]
-    rating = info[1]
-    restaurant_ratings[restaurant] = rating
+    restaurant_ratings = {}
 
+    for line in score_file:
+        line = line.rstrip()
+        restaurant_rating = line.split(":")
+        restaurant = restaurant_rating[0]
+        rating = restaurant_rating[1]
+        restaurant_ratings[restaurant] = rating
+        
+
+user_restaurant = raw_input("What is the restaurant name? ")
+user_rating = int(raw_input("Please rate this restaurant (1 - 5) "))
+restaurant_ratings[user_restaurant] = user_rating
 
 for restaurant, rating in sorted(restaurant_ratings.items()):
     print "{} is rated at {}.".format(restaurant, rating)
